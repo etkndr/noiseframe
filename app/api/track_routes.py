7bd217c92895ec5b6f8a432c0fbe4eb87c534ece
Eric Kinder <etkndr@gmail.com>
2023-08-17 19:21:23 -0400

    added track routes

--- app/api/track_routes.py
+++ app/api/track_routes.py
@@ -1,4 +1,103 @@
+from app.models import db
+from app.models.track import Track
+from app.models.song import Song
+from app.forms.track_form import TrackForm
 from flask import Blueprint, request
-from app.models import track
+from flask_login import current_user, login_required
+from .auth_routes import validation_errors_to_error_messages
 
 track_routes = Blueprint("track", __name__)
+
+# GET TRACKS FOR CURRENT SONG
+@track_routes.route("/")
+def song_tracks(id):
+    song = Song.query.get(id)
+    
+    if not song:
+        return {"errors": "Song not found"}, 404
+    
+    tracks = Track.query.filter(Track.song_id == id).all()
+    
+    return {"tracks": [track.to_dict() for track in tracks]}
+
+# GET TRACK BY ID
+@track_routes.route("/<int:id>")
+def get_track(id):
+    track = Track.query.get(id)
+    
+    if not track:
+        return {"errors": "Track not found"}, 404
+    
+    return track.to_dict()
+
+# SAVE NEW TRACK
+@track_routes.route("/", methods=["POST"])
+@login_required
+def new_track(id):
+    form = TrackForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    
+    song = Song.query.get(id)
+    if not song:
+        return {"errors": "Song not found"}, 404
+    if song.user_id != current_user.id:
+        return {"errors": "Tracks can only be added by song creator"}, 400
+    
+    if form.validate_on_submit():
+        track = Track(
+            song_id = id,
+            instrument_id = form.instrument_id,
+            title = form.title,
+            notes = form.notes,
+            volume = form.volume
+        )
+
+        db.session.add(track)
+        db.session.commit()
+        
+        return track.to_dict()
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
+
+# SAVE TRACK EDITS
+@track_routes.route("/<int:id>", methods=["PUT"])
+@login_required
+def edit_track(id, song_id):
+    track = Track.query.get(id)
+    if not track:
+        return {"errors": "Track not found"}, 404
+    
+    form = TrackForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    
+    song = Song.query.get(song_id)
+    if not song:
+        return {"errors": "Song not found"}, 404
+    if song.user_id != current_user.id:
+        return {"errors": "Tracks can only be edited by song creator"}, 400
+    
+    if form.validate_on_submit():
+        track.instrument_id = form.instrument_id
+        track.title = form.title
+        track.notes = form.notes
+        track.volume = form.volume
+
+        db.session.commit()
+        
+        return track.to_dict()
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
+
+# DELETE TRACK
+@track_routes.route("/<int:id>", methods=["DELETE"])
+@login_required
+def delete_track(id):
+    track = Track.query.get(id)
+    if not track:
+        return {"errors": "Track not found"}, 404
+    
+    song = Song.query.get(track.song_id)
+    if song.user_id != current_user.id:
+        return {"errors": "Tracks can only be deleted by their creator"}, 400
+    
+    db.session.delete(track)
+    
+    return {"message": "Track successfully deleted"}
--- app/forms/track_form.py
+++ app/forms/track_form.py
@@ -3,6 +3,7 @@ from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
 class TrackForm(FlaskForm):
+    instrument_id = IntegerField("Instrument", validators=[DataRequired()])
     title = StringField("Track title", validators=[DataRequired()])
     notes = StringField("Track notes")
     volume = IntegerField("Volume", validators=[DataRequired(), NumberRange(-10, -1)])
