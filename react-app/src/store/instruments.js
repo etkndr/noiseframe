efc537fa57ca89b4f3068790f63b84e511c53805
Eric Kinder <etkndr@gmail.com>
2023-08-20 23:34:01 -0400

    crud for instruments and tracks

--- react-app/src/store/instruments.js
+++ react-app/src/store/instruments.js
@@ -1,6 +1,8 @@
 const GET_INSTRUMENTS = "instruments/GET_INSTRUMENTS"
 const GET_INSTRUMENT = "instruments/GET_INSTRUMENT"
 const SAVE_INSTRUMENT = "instruments/SAVE_INSTRUMENT"
+const EDIT_INSTRUMENT = "instruments/EDIT_INSTRUMENT"
+const DELETE_INSTRUMENT = "instruments/DELETE_INSTRUMENTS"
 
 function loadInstruments(insts) {
     return {
@@ -19,6 +21,20 @@ function loadInstrument(inst) {
 function newInstrument(inst) {
     return {
         type: SAVE_INSTRUMENT,
+        inst
+    }
+}
+
+function saveEdit(inst) {
+    return {
+        type: EDIT_INSTRUMENT,
+        inst
+    }
+}
+
+function dltInstrument(inst) {
+    return {
+        type: DELETE_INSTRUMENT,
         inst
     }
 }
@@ -61,6 +77,32 @@ export const saveInstrument = (inst) => async dispatch => {
     }
 }
 
+const editInstrument = (id, inst) => async dispatch => {
+    const res = await fetch(`/api/instruments/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(inst)
+    })
+
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(saveEdit(data))
+        return data
+    }
+}
+
+const deleteInstrument = (id) => async dispatch => {
+    const res = await fetch(`/api/instruments/${id}`, {
+        method: "DELETE"
+    })
+
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(dltInstrument(data))
+        return data
+    }
+}
+
 const initState = {}
 
 export default function instruments(state = initState, action) {
@@ -73,6 +115,12 @@ export default function instruments(state = initState, action) {
             return newState
         case GET_INSTRUMENT:
             newState[action.inst.id] = action.inst
+            return newState
+        case EDIT_INSTRUMENT:
+            newState[action.inst.id] = action.inst
+            return newState
+        case DELETE_INSTRUMENT:
+            delete newState[action.inst.id]
             return newState
         default:
             return state
