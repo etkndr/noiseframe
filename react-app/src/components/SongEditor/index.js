86ea77e3796812426cd6a70b296e09e3257e6039
Eric Kinder <103692901+etkndr@users.noreply.github.com>
2023-09-04 22:35:56 -0400

    Update index.js

--- react-app/src/components/SongEditor/index.js
+++ react-app/src/components/SongEditor/index.js
@@ -1,56 +1,109 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min"
+import { NavLink, useParams } from "react-router-dom/cjs/react-router-dom.min"
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import * as trackActions from "../../store/tracks"
 import * as songActions from "../../store/songs"
-import {getInstruments} from "../../store/instruments"
-import {Instrument, Song, Track} from "reactronica"
-import Tracks from "../Tracks"
+import { getInstruments } from "../../store/instruments"
+import * as sampleActions from "../../store/samples"
+import {Song} from "reactronica"
+import Sequencer from "../Sequencer"
 
 export default function SongEditor() {
     const dispatch = useDispatch()
     const {id} = useParams()
-    const tracks = useSelector(state => state.tracks)
+    const currUser = useSelector(state => state.session.user)
     const song = useSelector(state => state.songs)
-    const insts = useSelector(state => state.instruments)
+    const allInst = useSelector(state => state.instruments)
+    const samples = useSelector(state => Object.values(state.samples))
     const [play, setPlay] = useState(false)
-    const [focus, setFocus] = useState(false)
+    
+    const [currInst, setCurrInst] = useState("")
+    const [title, setTitle] = useState(song?.title)
+    const [bpm, setBpm] = useState(song?.bpm)
 
+    useEffect(() => {
+        dispatch(songActions.getSong(id))
+    }, [dispatch])
+
+    useEffect(() => {
+        dispatch(getInstruments())
+        
+        if (song?.instrument_id) {
+            setCurrInst(song?.instrument_id)
+        }
+    }, [song])
     
     useEffect(() => {
-        dispatch(trackActions.getTracks(id))
-        dispatch(songActions.getSong(id))
-        dispatch(getInstruments())
-    }, [])
+        dispatch(sampleActions.getSamples(currInst))
+    }, [currInst])
     
-    const [title, setTitle] = useState("")
-    const [bpm, setBpm] = useState(120)
-    
-    function save(e) {
+    function saveSong(e) {
         e.preventDefault()
-        const newSong = {title, bpm}
+        const newSong = {
+            title,
+            bpm,
+            instrument_id: currInst
+        }
         const save = dispatch(songActions.editSong(id, newSong))
         console.log("success")
     }
-    
+
     function dltSong(e) {
         e.preventDefault()
         const dlt = dispatch(songActions.deleteSong(id))
         console.log("success")
     }
-        
+
+    function saveSample(sample, stepArr) {
+        const {id, name, url} = sample
+        const save = dispatch(sampleActions.saveSample(id, {
+            name,
+            steps: stepArr,
+            url
+        }))
+    }
+
+    console.log(currInst)
+
+    if (song?.user_id !== currUser?.id || !currUser) {
+        return "Unauthorized"
+    }
     return (
         <>
-            song editor
-            <button onClick={() => setPlay(!play)}>start/stop</button>
-            <form onSubmit={save}>
-                <input onChange={(e) => setTitle(e.target.value)} onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} placeholder="title" value={title}/>
-                <input onChange={(e) => setBpm(e.target.value)} placeholder="BPM"/>
+            <h2>{song?.title}</h2>
+            {currInst &&
+            <h3>
+                <NavLink to={`/instruments/${currInst}`}>
+                    {allInst[currInst]?.title}
+                </NavLink>
+            </h3>
+            }
+            {currInst &&
+            <button onClick={() => setPlay(!play)}>{!play && "play"}{play && "stop"}</button>
+            }
+            <form onSubmit={saveSong}>
+                <select onChange={(e) => setCurrInst(e.target.value)} value={currInst}>
+                    <option selected disabled>select an instrument</option>
+                    {Object.values(allInst)?.map((inst, idx) => {
+                        return <option value={inst.id} key={idx}>{inst.title}</option>
+                    })}
+                </select>
+                <label for="title">song title</label>
+                    <input onChange={(e) => setTitle(e.target.value)} placeholder={`${song?.title}`} name="title"/>
+                <label for="bpm">bpm</label>
+                    <input onChange={(e) => setBpm(e.target.value)} placeholder={`${song?.bpm}`} name="bpm"/>
                 <button type="submit">save</button>
                 <button onClick={dltSong}>delete</button>
             </form>
-            <Song isPlaying={play} bpm={bpm}>
-                <Tracks songId={id} focus={focus}/>
+            <Song bpm={bpm*2 || 240} isPlaying={play}>
+                {samples?.map((sample, idx) => {
+                    return <Sequencer 
+                                url={sample.url}
+                                sample={sample} 
+                                savedSteps={sample.steps} 
+                                saveSample={saveSample} 
+                                key={idx} 
+                            />
+                })}
             </Song>
         </>
     )
