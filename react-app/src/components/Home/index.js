7236e87c840e239cdfbda95c4d4d3f0e9a1c82ba
Eric Kinder <etkndr@gmail.com>
2023-09-09 12:48:57 -0400

    homepage init

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,9 +1,10 @@
-import styles from "./Home.module.css"
+import "./Home.css"
 import * as songActions from "../../store/songs"
 import * as instrumentActions from "../../store/instruments"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import classnames from "classnames"
 
 export default function Home() {
     const history = useHistory()
@@ -19,23 +20,39 @@ export default function Home() {
         dispatch(instrumentActions.getInstruments())
     }, [dispatch])
 
+    
+
     return (
         <>
-            <select onChange={(e) => setSelSong(e.target.value)}>
-                <option selected disabled>song list</option>
+        <div className="lists">
+            <div className="list">
                 {songs?.map((song, idx) => {
-                    return <option value={song.id} key={idx}>{song.title}</option>
+                    let select = ""
+                    if (song.id === selSong) {
+                        select = "select"
+                    }
+                    return <li 
+                        onClick={() => setSelSong(song.id)} 
+                        className={select}
+                        key={idx}>
+                    {song.title}</li>
                 })}
-            </select>
+            </div>
             <button onClick={() => selSong > 0 && history.push(`/songs/${selSong}`)}>edit song</button>
-            <select onChange={(e) => setSelInst(e.target.value)}>
-                <option selected disabled>instrument list</option>
+            <div className="list">
                 {insts?.map((inst, idx) => {
-                    return <option value={inst.id} key={idx}>{inst.title}</option>
+                    let select = ""
+                    if (inst.id === selInst) {
+                        select = "select"
+                    }
+                    return <li 
+                        onClick={() => setSelInst(inst.id)} 
+                        className={select}
+                        key={idx}>{inst.title}</li>
                 })}
-            </select>
+            </div>
             <button onClick={() => selInst > 0 && history.push(`/instruments/${selInst}`)}>edit instrument</button>
-
+            </div>
             <div>
                 <button onClick={() => history.push("/songs")}>new song</button>
                 <button onClick={() => history.push("/instruments")}>new instrument</button>
